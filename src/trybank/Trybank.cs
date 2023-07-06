e52617888048aadf1a6372411286bda5d64b8d89
Priscila <priscilasartori2@gmail.com>
2023-07-06 11:46:43 -0300

    feat: CheckBalance

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -86,7 +86,14 @@ public class Trybank
   // 4. Construa a funcionalidade de checar o saldo
   public int CheckBalance()
   {
-    throw new NotImplementedException();
+    if (!Logged)
+    {
+      throw new AccessViolationException("Usuário não está logado");
+    }
+    else
+    {
+      return Bank[registeredAccounts, 3];
+    }
   }
 
   // 5. Construa a funcionalidade de depositar dinheiro

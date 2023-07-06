2ad061dc9c8c045d4943580f8ebb3db464da5224
Priscila <priscilasartori2@gmail.com>
2023-07-06 12:02:39 -0300

    feat: Transfer

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -118,18 +118,43 @@ public class Trybank
     }
     else
     {
-      Bank[loggedUser, 3] -= value;
+      if (Bank[loggedUser, 3] < value)
+      {
+        throw new InvalidOperationException("Saldo insuficiente");
+      }
+      else
+      {
+        Bank[loggedUser, 3] -= value;
+      }
     }
 
-    if (Bank[loggedUser, 3] < 0)
-    {
-      throw new InvalidOperationException("Saldo insuficiente");
-    }
   }
 
   // 7. Construa a funcionalidade de transferir dinheiro entre contas
   public void Transfer(int destinationNumber, int destinationAgency, int value)
   {
-    throw new NotImplementedException();
+    if (!Logged)
+    {
+      throw new AccessViolationException("Usuário não está logado");
+    }
+    else
+    {
+      if (Bank[loggedUser, 3] < value)
+      {
+        throw new InvalidOperationException("Saldo insuficiente");
+      }
+      else
+      {
+        for (int i = 0; i < registeredAccounts; i++)
+        {
+          if (Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+          {
+            Bank[loggedUser, 3] -= value;
+            Bank[i, 3] += value;
+          }
+        }
+      }
+    }
+
   }
 }
